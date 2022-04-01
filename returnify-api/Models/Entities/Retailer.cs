3773693f680bbf132a0a791b040ec51feec181cf
Burhan Faquiri <59700142+burhanf@users.noreply.github.com>
2022-03-31 20:09:08 -0400

    Creates models that a Retailer will use Will become a db table each

---
+++ returnify-api/Models/Entities/Retailer.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace returnify_api.Models.Entities
+{
+    public class Retailer
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Location { get; set; }
+        public String RetailerType { get; set; }
+        public List<Return> Returns { get; set; }
+
+    }
+}
